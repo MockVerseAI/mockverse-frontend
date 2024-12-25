6d7969b9e8f9897b0ff895bd482419e0ef6e20dd
ayushsaha1018 <ayushsaha111@gmail.com>
2024-12-25 18:23:00 +0530

    feat: update avatar

--- src/components/Sidebar/nav-user.tsx
+++ src/components/Sidebar/nav-user.tsx
@@ -50,7 +50,7 @@ export function NavUser({ user }: { user: User | null }) {
             >
               <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user?.avatar.url} alt={user?.username} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <AvatarFallback className="rounded-lg uppercase">{user?.username?.slice(0, 1)}</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user?.username}</span>
--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,17 @@ import Providers from "./components/Providers.tsx";
 import "./index.css";
 import DashboardLayout from "./layouts/DashboardLayout.tsx";
 import PublicLayout from "./layouts/PublicLayout.tsx";
-import Account from "./pages/Account.tsx";
-import DashboardHome from "./pages/DashboardHome.tsx";
-import EmailSent from "./pages/EmailSent.tsx";
-import ForgotPassword from "./pages/ForgotPassword.tsx";
+import Account from "./pages/dashboard/Account.tsx";
+import DashboardHome from "./pages/dashboard/DashboardHome.tsx";
+import EmailSent from "./pages/auth/EmailSent.tsx";
+import ForgotPassword from "./pages/auth/ForgotPassword.tsx";
 import Home from "./pages/Home.tsx";
 import Interview from "./pages/Interview.tsx";
-import Login from "./pages/Login.tsx";
-import ResetPassword from "./pages/ResetPassword.tsx";
-import Signup from "./pages/Signup.tsx";
-import SSOSuccess from "./pages/SSOSuccess.tsx";
-import VerifyEmail from "./pages/VerifyEmail.tsx";
+import Login from "./pages/auth/Login.tsx";
+import ResetPassword from "./pages/auth/ResetPassword.tsx";
+import Signup from "./pages/auth/Signup.tsx";
+import SSOSuccess from "./pages/auth/SSOSuccess.tsx";
+import VerifyEmail from "./pages/auth/VerifyEmail.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
