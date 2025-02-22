831af68d408e41810e760c020dac394c3d320a1d
Ayush Kumar <ayushsaha111@gmail.com>
2025-02-22 17:19:29 +0530

    feat: reset password (#19)

--- src/pages/auth/ForgotPassword.tsx
+++ src/pages/auth/ForgotPassword.tsx
@@ -2,16 +2,22 @@ import AuthScreenWrapper from "@/components/AuthScreenWrapper";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import UserService from "@/services/userService";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Label } from "@radix-ui/react-label";
+import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
+import { useState } from "react";
 
 const formSchema = z.object({
   email: z.string().email(),
 });
 
 const ForgotPassword = () => {
+  const [isEmailSent, setIsEmailSent] = useState(false);
+
   const { handleSubmit, register } = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -19,10 +25,19 @@ const ForgotPassword = () => {
     },
   });
 
+  const { mutate: forgotPassword, isPending } = useMutation({
+    mutationFn: (payload: z.infer<typeof formSchema>) => UserService.forgotPassword(payload),
+    onError: (e: any) => {
+      const errObj = e?.response?.data;
+      toast.error(errObj.message);
+    },
+    onSuccess: () => {
+      setIsEmailSent(true);
+    },
+  });
+
   const onSubmit = (values: z.infer<typeof formSchema>) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    forgotPassword(values);
   };
 
   return (
@@ -30,22 +45,29 @@ const ForgotPassword = () => {
       <Card>
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Forgot Password</CardTitle>
-          <CardDescription>Enter your email to reset password</CardDescription>
+          <CardDescription>{isEmailSent ? "Check your email" : "Enter your email to reset password"}</CardDescription>
         </CardHeader>
         <CardContent>
-          <div>
+          {isEmailSent ? (
+            <div className="space-y-4 text-center">
+              <p className="text-muted-foreground">
+                We have sent password reset instructions to your email address. Please check your inbox and follow the
+                instructions to reset your password.
+              </p>
+            </div>
+          ) : (
             <div className="grid gap-6">
               <form onSubmit={handleSubmit(onSubmit)} className="grid gap-6">
                 <div className="grid gap-2">
                   <Label htmlFor="email">Email</Label>
                   <Input id="email" type="email" placeholder="m@example.com" {...register("email")} required />
                 </div>
-                <Button type="submit" className="w-full">
+                <Button disabled={isPending} isLoading={isPending} type="submit" className="w-full">
                   Submit
                 </Button>
               </form>
             </div>
-          </div>
+          )}
         </CardContent>
       </Card>
     </AuthScreenWrapper>
