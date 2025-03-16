456466302be2bb28dc3685c7833343c2e6823d6b
ayushsaha1018 <ayushsaha111@gmail.com>
2025-03-16 22:59:57 +0530

    feat: add interview workspace functionality

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -23,8 +23,8 @@ const navItems = [
     icon: Home,
   },
   {
-    name: "Interview",
-    url: "/dashboard/interview",
+    name: "Interview Spaces",
+    url: "/dashboard/interview-workspace",
     icon: ContactRound,
   },
   {
@@ -45,7 +45,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           size="lg"
           className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
         >
-          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-sidebar-primary-foreground">
+          <div className="bg-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
             <BrainCircuit className="size-4" />
           </div>
           <div className="grid flex-1 text-left text-sm leading-tight">
