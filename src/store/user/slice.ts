04d3418dc705eda3accc95294f1475dc5eff45b3
ayushsaha1018 <ayushsaha111@gmail.com>
2025-05-29 23:22:57 +0530

    feat: changes

--- src/store/user/slice.ts
+++ src/store/user/slice.ts
@@ -1,6 +1,7 @@
 import { Resume, User } from "@/types";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { getAllResumes, getUser } from "./actions";
+import socketService from "@/services/socketService";
 
 interface UserState {
   user: User | null;
@@ -18,6 +19,7 @@ const userSlice = createSlice({
   reducers: {
     setUser(state, action: PayloadAction<User>) {
       state.user = action.payload;
+      socketService.connect(action.payload._id);
     },
     editUser(state, action: PayloadAction<Partial<User>>) {
       if (state.user) {
@@ -29,11 +31,15 @@ const userSlice = createSlice({
     },
     removeUser(state) {
       state.user = null;
+      socketService.disconnect();
     },
   },
   extraReducers: (builder) => {
     builder.addCase(getUser.fulfilled, (state, action) => {
-      state.user = action.payload;
+      if (action.payload) {
+        state.user = action.payload;
+        socketService.connect(action.payload._id);
+      }
     });
     builder.addCase(getAllResumes.fulfilled, (state, action) => {
       state.resumes = action.payload;
